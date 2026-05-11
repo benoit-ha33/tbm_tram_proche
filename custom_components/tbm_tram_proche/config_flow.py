1293bbf5718185726aa8f3d137fd568f06ab444d
Benoît COSQUER <benoitcosquer@Mac.lan>
2026-05-11 11:33:18 +0200

    Fix config flow

--- custom_components/tbm_tram_proche/config_flow.py
+++ custom_components/tbm_tram_proche/config_flow.py
@@ -1,8 +1,22 @@
 import voluptuous as vol
+
 from homeassistant import config_entries
 
-class TBMTramProcheConfigFlow(config_entries.ConfigFlow, domain="tbm_tram_proche"):
+from .const import DOMAIN
+
+
+class TBMTramProcheConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        return self.async_create_entry(title="TBM Tram Proche", data={})
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="TBM Tram Proche",
+                data={}
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({})
+        )
